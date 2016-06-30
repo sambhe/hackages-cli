bd914453e1034660a6757a90ee6581adb0431abf
davyengone <engonenzedavy@yahoo.com>
2016-06-30 23:48:28 +0200

    fixed path in version for package.json

--- tools/parser/index.js
+++ tools/parser/index.js
@@ -1,28 +1,20 @@
 /**
 * CLI tools: Command line tool
 **/
-import ci from './ci';
+// import ci from './ci';
 import live from './live';
-import deploy from './deploy';
+// import deploy from './deploy';
 import karma from './karma';
-import eslint from './linter';
+// import eslint from './linter';
 import version from './version';
-import webpack from './webpack';
+import build from './webpack';
 import cliparse from 'cliparse';
 import watchFiles from './watchFiles';
 
 const cliParser = cliparse.cli({
   name: 'hackages|hack <command> [options]',
   description: 'hackages not hackage like Haskell',
-  commands: [
-    webpack,
-    eslint,
-    watchFiles,
-    ci,
-    karma,
-    live,
-    deploy,
-  ],
+  commands: [live, build, watchFiles, karma],
   version: version(),
 });
 
